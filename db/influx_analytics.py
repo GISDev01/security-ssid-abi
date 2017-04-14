677ea46faf5fa2da1aa3aff023fdac648496420c
gisdev01 <gisdev01@gmail.com>
2017-04-13 20:39:00 -0400

    Start influxdb metrics on hits

--- db/influx_analytics.py
+++ db/influx_analytics.py
@@ -1,4 +1,25 @@
 from db import influxdb_client
 
-influxdb_client.query('*')
+query = """
+        SHOW TAG VALUES FROM "clientdevices" WITH KEY IN ("probedssid")
+        """
+result_set = influxdb_client.query(query)
+print("Result: {0}".format(result_set))
+
+for result in result_set:
+    ssid_list = [ssid['value'] for ssid in result]
+
+print ssid_list
+for ssid in ssid_list:
+    ssid = "'" + ssid.replace("'", r"\'") + "'"
+    ssid_query = 'SELECT * FROM "clientdevices" WHERE "probedssid" = ' + ssid
+    print(ssid_query)
+    ssid_results = influxdb_client.query(ssid_query)
+    print("ssid_results: {0}".format(ssid_results))
+
+
+
+
+
+
 
