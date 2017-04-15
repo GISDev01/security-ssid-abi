3c146f10c3150f3025647625c17eb7d80de544fa
gisdev01 <gisdev01@gmail.com>
2017-04-14 20:49:00 -0400

    Add InfluxDB MEAN calculation for RSSI on a specific probed ssid for all clients

--- db/influx_analytics.py
+++ db/influx_analytics.py
@@ -10,16 +10,24 @@ for result in result_set:
     ssid_list = [ssid['value'] for ssid in result]
 
 print ssid_list
+
+# Loop through all of the detected SSID names in the DB for all timeframes
 for ssid in ssid_list:
     ssid = "'" + ssid.replace("'", r"\'") + "'"
     ssid_query = 'SELECT * FROM "clientdevices" WHERE "probedssid" = ' + ssid
-    print(ssid_query)
-    ssid_results = influxdb_client.query(ssid_query)
-    print("ssid_results: {0}".format(ssid_results))
+    #print(ssid_query)
+    #ssid_results = influxdb_client.query(ssid_query)
+    #print("ssid_results: {0}".format(ssid_results))
 
-
-
-
+# Hardcoded test for dynamic MEAN calc on RSSI
+ssid_name = 'linksys'
+ssid_name = "'" + ssid_name.replace("'", r"\'") + "'"
+field_name = 'rssi'
+mean_field =  field_name.replace("'", r"\'")
+ssid_test_query_mean = 'SELECT MEAN("' + mean_field + '") FROM "clientdevices" WHERE "probedssid" = ' + ssid_name
+print(ssid_test_query_mean)
+ssid_test_results = influxdb_client.query(ssid_test_query_mean)
+print("ssid_results: {0}".format(ssid_test_results))
 
 
 
