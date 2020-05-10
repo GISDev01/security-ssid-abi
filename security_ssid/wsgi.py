df9a8641fa4023656cfec4faea732dd0d10b028c
gisdev01 <gisdev01@gmail.com>
2020-05-10 18:57:55 -0400

    Update to be compatible with updated version of Django

--- security_ssid/wsgi.py
+++ security_ssid/wsgi.py
@@ -1,29 +1,16 @@
 """
-WSGI config for security-ssid-abi project.
+WSGI config for security_ssid project.
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
+
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "security_ssid.settings")
-
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'security_ssid.settings')
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+application = get_wsgi_application()
